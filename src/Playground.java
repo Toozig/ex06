80a726f1ee10c0a75190b4af8bf146723b16a506
Toozig <35314420+Toozig@users.noreply.github.com>
2018-06-17 17:26:36 +0300

    parser and main creator

---
+++ src/Playground.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.lang.*;
+
+import java.util.List;
+
+
+
+public class Playground {
+
+
+    public static void main(String[] args) throws ClassNotFoundException, src.MyExceptions, IOException {
+        int x  = 3;
+        Parser p = new Parser("Files/Moodle Example/playg");
+         List<String> pr = p.checker();
+
+        File pg = new File("Files/Moodle Example/playg");
+        List<String> original = p.convertToStringArr("Files/Moodle Example/playg");
+        BufferedWriter writer = new BufferedWriter(new FileWriter("Files/Moodle Example/playgRESULT"));
+
+        for (int i = 0; i < pr.size(); i++) {
+            String orLine = original.get(i);
+            String VerLine = pr.get(i);
+            writer.write(orLine + " ---> " + VerLine);
+            writer.newLine();
+
+        }
+        writer.close();
+    }
+
+
+
+}
