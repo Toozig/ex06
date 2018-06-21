491bf989cd231fc45ab7450cbf714aee8774ea9a
Toozig <35314420+Toozig@users.noreply.github.com>
2018-06-21 17:01:22 +0300

    455 / 460

--- src/oop/ex6/Playground.java
+++ src/oop/ex6/Playground.java
@@ -11,46 +11,46 @@ import java.util.regex.Pattern;
 public class Playground {
 
 
-    public static void main(String[] args) throws ClassNotFoundException, MyExceptions, IOException {
-        String line = " a = \"hey,Ijust met you\", b = \"and this is crazy\"";
-        String ptrn = "(\\S*(\\s*=\\s*(\\\".*\\\")|\\S*)?\\s*)\\,";
+    private static String[] singelVarArrCreator(String expression) throws MyExceptions {
+        expression = expression.trim();
+        String ptrn = "([A-Za-z0-9_]*)\\s*(=\\s*([A-Za-z0-9_]+))\\s*";
         Pattern pattern = Pattern.compile(ptrn);
-        Matcher matcher = pattern.matcher(line);
-        matcher.find();
-        System.out.println(matcher.group(1));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        String a = "\'c\'";
-        int x  = 3;
-        Parser p = new Parser("Files/Moodle Example/playg");
-
-        boolean adsad = p.isTypeMatch(a,"char");
-
-        File pg = new File("Files/Moodle Example/playg");
-        List<String> original = p.convertToStringArr("Files/Moodle Example/playg");
-        BufferedWriter writer = new BufferedWriter(new FileWriter("Files/Moodle Example/playgRESULT"));
-
-        for (String orLine : original) {
-            String VerLine = p.lineDefining(orLine);
-            writer.write(orLine + " ---> " + VerLine);
-            writer.newLine();
-
+        Matcher matcher = pattern.matcher(expression);
+        if(!matcher.matches()) {
+            throw  new MyExceptions("Invalid line of var creation");
         }
-        writer.close();
+        return  new String[] {matcher.group(1), matcher.group(3)};
     }
 
 
-
+    public static void main(String[] args) throws ClassNotFoundException, MyExceptions, IOException {
+        String line = "ads dfs   = fgfdgfd ";
+        String[] dafdas = singelVarArrCreator(line);
+    }
 }
+//        System.out.println(matcher.group(1));
+
+
+//
+//        String a = "\'c\'";
+//        int x  = 3;
+//        Parser p = new Parser("Files/Moodle Example/playg");
+//
+//        boolean adsad = p.isTypeMatch(a,"char");
+//
+//        File pg = new File("Files/Moodle Example/playg");
+//        List<String> original = p.convertToStringArr("Files/Moodle Example/playg");
+//        BufferedWriter writer = new BufferedWriter(new FileWriter("Files/Moodle Example/playgRESULT"));
+//
+//        for (String orLine : original) {
+//            String VerLine = p.lineDefining(orLine);
+//            writer.write(orLine + " ---> " + VerLine);
+//            writer.newLine();
+//
+//        }
+//        writer.close();
+//    }
+//
+//
+//
+//}
